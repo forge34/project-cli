5ee40b48c1ceae4fdef060030adabe793af34cba
Forge <28455546+forge34@users.noreply.github.com>
2026-01-16 23:28:41 +0200

    refactor: rename cli to cli_helpers for clarity

--- internals/cli_helpers.go
+++ internals/cli_helpers.go
@@ -5,11 +5,11 @@ import "fmt"
 func PrintList(templates []string) {
 	fmt.Println(Header.Render("Available Templates:"))
 	for _, t := range templates {
-		fmt.Println(Success.Render( " • " ) + t)
+		fmt.Println(Success.Render(" • ") + t)
 	}
 }
 
 func PrintCreate(template, dest string) {
-	fmt.Println(Success.Render("✔ Project created successfully!"))
+	fmt.Println(Success.Render("✔ Project created successfully!\n\n"))
 	fmt.Println(InfoBox.Render(fmt.Sprintf("Template: %s\nPath: %s", template, dest)))
 }
